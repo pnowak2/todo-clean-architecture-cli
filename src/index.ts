65aaf5c8654b4413c5873d9bdee7bdb65c1c03c3
Piotr Nowak <p.nowak2@gmail.com>
2020-09-24 11:15:45 +0200

    fix(cli): simplify prompt

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import figlet from 'figlet';
 import inquirer from 'inquirer';
 import Table from 'cli-table';
 import { combineLatest } from 'rxjs';
+import { skip } from 'rxjs/operators';
 
 // inquirer
 //   .prompt([
@@ -29,7 +30,7 @@ const todoApp: TodoPresenter = new TodoDefaultPresenter(new TodoInMemoryReposito
   { id: '3', title: 'baz', completed: true },
 ]));
 
-combineLatest(todoApp.todos$, todoApp.activeTodosCount$).subscribe(([tso, count]) => {
+combineLatest(todoApp.todos$, todoApp.activeTodosCount$).pipe(skip(1)).subscribe(([tso, count]) => {
   clear();
 
   console.log(
@@ -53,34 +54,17 @@ combineLatest(todoApp.todos$, todoApp.activeTodosCount$).subscribe(([tso, count]
   console.log(table.toString());
 });
 
-todoApp.getActiveTodos();
-todoApp.getCompletedTodos();
 todoApp.getAllTodos();
-// todoApp.markTodoAsActive('3');
-// todoApp.addTodo('a');
-// todoApp.addTodo('b');
 
 program
   .version('0.0.1')
-  .description('An example CLI for ordering pizza\'s')
-  .option('-p, --peppers', 'Add peppers')
-  .option('-P, --pineapple', 'Add pineapple')
-  .option('-b, --bbq', 'Add bbq sauce')
-  .option('-c, --cheese <type>', 'Add the specified type of cheese [marble]')
-  .option('-C, --no-cheese', 'You do not want any cheese')
+  .description('Todo CLI')
+  .option('-a, --all', 'Show ALL todos')
   .parse(process.argv);
 
-console.log('you ordered a pizza with:');
+console.log('You selected todos with options');
 
-if (program.peppers) { console.log('  - peppers'); }
-if (program.pineapple) { console.log('  - pineapple'); }
-if (program.bbq) { console.log('  - bbq'); }
-
-const cheese: string = true === program.cheese
-  ? 'marble'
-  : program.cheese || 'no';
-
-console.log('  - %s cheese', cheese);
+if (program.all) { console.log('  - all'); }
 
 if (!process.argv.slice(2).length) {
   program.outputHelp();
